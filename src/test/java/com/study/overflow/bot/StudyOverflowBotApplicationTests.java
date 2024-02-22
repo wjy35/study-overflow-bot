fc591a11b7e78270694b06ac48b657cbaab0dfdb
왕준영 <wangsun7@naver.com>
2024-02-22 15:07:08 +0900

    Feat: init

---
+++ src/test/java/com/study/overflow/bot/StudyOverflowBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.overflow.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyOverflowBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
